672aecec1f5b4242b21fc7a3bc7296f20911480a
ragul-onemodo <ragul@onemodo.com>
2026-05-12 06:50:39 +0530

    build: update production assets with latest frontend compilation

---
+++ public/build/assets/index-BM5Io3LS.js
@@ -0,0 +1,1 @@
+import{s as e}from"./index-C01R_oDu.js";var o={name:"InputSwitch",extends:e,mounted:function(){console.warn("Deprecated since v4. Use ToggleSwitch component instead.")}};export{o as s};
---
+++ public/build/assets/index-iZe37KSC.js
@@ -0,0 +1,1 @@
+import{b5 as r}from"./app-C00g7FWR.js";var s=r();export{s as O};
