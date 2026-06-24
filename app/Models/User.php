5417654ee1f7377e9f5a508d6a68c4b50ff97240
Cipher <skkiran153@gmail.com>
2026-06-24 15:44:44 +0530

    Mail turned off for user creation

--- app/Models/User.php
+++ app/Models/User.php
@@ -153,7 +153,7 @@ class User extends Authenticatable implements MustVerifyEmail
 			$user = self::create($data);
 
             // Dispatch Registered event to trigger email verification notification
-            event(new \Illuminate\Auth\Events\Registered($user));
+            // event(new \Illuminate\Auth\Events\Registered($user));
 
 			if (isset($data['entity_users']) && is_array($data['entity_users']) && count($data['entity_users']) > 0) {
 				foreach ($data['entity_users'] as $eu) {
